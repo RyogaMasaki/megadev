75201e48e28a6df06f1a0666fd2b9ec2b258dd60
Damian Rogers <damian@sudden-desu.net>
2017-10-25 00:12:34 -0400

    Checkpoint

--- lib/boot.s
+++ lib/boot.s
@@ -5,6 +5,9 @@
 
 	.org 0x00000000
 
+__START:
+	.ascii	"<========"
+
 _m68kVectorTable:
 	dc.l	0x00fffe00			/* Stack address */
 	dc.l	_entryPoint			/* Program start */
@@ -26,7 +29,9 @@ _m68kVectorTable:
 	dc.l	_ex, _ex, _ex, _ex, _ex, _ex
 
 	/* Interrupt Requests (IRQ) */
-	dc.l	_irq1, _irq2, _irq3
+	dc.l	_irq1
+	dc.l	_extInt			/* IRQ2 - External Interrupt */
+	dc.l	_irq3
 	dc.l	_hBlankInt		/* IRQ4 - Horizontal Blank */
 	dc.l	_irq5
 	dc.l	_vBlankInt		/* IRQ6 - Vertical Blank */
@@ -43,13 +48,121 @@ _m68kVectorTable:
 	dc.l	_ex, _ex, _ex, _ex, _ex, _ex
 	dc.l	_ex, _ex, _ex, _ex
 
+/* Standard M68k exceptions */
+_errBus:			/* Bus error */
+	jmp _ex
+
+_errAddr:			/* Address error */
+	jmp _ex
+
+_errIllegal:		/* Illegal instruction */
+	jmp _ex
+
+_errZero:			/* Divide by zero */
+	jmp _ex
+
+_errChkInst:		/* CHK instruction */
+	jmp _ex
+
+_errTrapV:			/* TRAPV instruction */
+	jmp _ex
+
+_errPriv:			/* Privilege violation */
+	jmp _ex
+
+_errTrace:			/* Trace */
+	jmp _ex
+
+_errLine1010:		/* Line 1010 emulator */
+	jmp _ex
+
+_errLine1111:		/* Line 1111 emulator */
+	jmp _ex
+
+/* Interrupt Requests */
+_irq1:				/* Unused */
+	jmp _ex
+
+_extInt:			/* External interrupt */
+	rte
+
+_irq3:				/* Unused */
+	jmp _ex
+
+_hBlankInt:			/* Horizontal Blank interrupt */
+	rte
+
+_irq5:				/* Unused */
+	jmp _ex
+
+_vBlankInt:			/* Vertical Blank interrupt */
+	rte
+
+_irq7:				/* Unused */
+	jmp _ex
+
+/* TRAP */
+
+_trap0:
+	rte
+
+_trap1:
+	rte
+
+_trap2:
+	rte
+
+_trap3:
+	rte
+
+_trap4:
+	rte
+
+_trap5:
+	rte
+
+_trap6:
+	rte
+
+_trap7:
+	rte
+
+_trap8:
+	rte
+
+_trap9:
+	rte
+
+_trapA:
+	rte
+
+_trapB:
+	rte
+
+_trapC:
+	rte
+
+_trapD:
+	rte
+
+_trapE:
+	rte
+
+_trapF:
+	rte
+
+_ex:				/* Generic exception handler */
+
+	jmp _ex
+
 ProgHeader:
 	.include "proghead"
 
 _entryPoint:
 	/* Begin hardware init */
 	move	#0x2700,%sr
-
+mainLoop:
+	jmp mainLoop
 
 __END:
-	
+	.ascii	"========>"
